b4432414b9a8ba3d7f78d494a53feaa899b41340
Emmanuel Regaudie <emmanuelregaudie5@gmail.com>
2025-09-15 10:42:50 +0200

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/R3.12_TD5",
 });
